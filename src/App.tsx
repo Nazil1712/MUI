e50a1af9599bbc878e43c3e59825bce5f0e483f4
Nazil1712 <ndhalwala@gmail.com>
2026-05-06 12:31:46 +0530

    Customizations using sx prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,56 @@ import { AutoAwesomeRounded } from "@mui/icons-material";
 import { Button, Container, Slider, Stack, TextField } from "@mui/material";
 
 function App() {
-  return (
+  /* return (
     <Container maxWidth="md">
       <Stack spacing={2}>
         <TextField label="Name or Email" />
         <Slider/>
         <Button startIcon={<AutoAwesomeRounded/>} variant="contained">Submit</Button>
+      </Stack>
+    </Container>
+  ); */
+
+  /* Customizing with sx={{}} */
+  return (
+    <Container
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Stack spacing={2}>
+        <Slider
+          /* sx={{
+            width: 100,
+            color: "success.main",
+            ":hover":{
+              color:"red"
+            },
+            "& .MuiSlider-thumb": {
+              color: "blue",
+              ":hover" : {
+                color:"white"
+              }
+            },
+          }} */
+
+        disabled
+        sx={{
+          width: 100,
+          /* "&.Mui-disabled":{
+            ".MuiSlider-thumb":{
+              color:"green"
+            }
+          } */
+         "&.Mui-disabled .MuiSlider-rail":{
+          color:"green"
+         }
+        }}
+
+        />
       </Stack>
     </Container>
   );
