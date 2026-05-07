9ae2f1ec4573829eb53b7686bf8b2e76b95821d2
Nazil1712 <ndhalwala@gmail.com>
2026-05-07 15:43:51 +0530

    CSS Global Overrides

--- src/Components/CustomizedComponent.tsx
+++ src/Components/CustomizedComponent.tsx
@@ -1,4 +1,13 @@
-import { styled, Slider, type SliderProps, createTheme, Button, ThemeProvider } from "@mui/material";
+import {
+  styled,
+  Slider,
+  type SliderProps,
+  createTheme,
+  Button,
+  ThemeProvider,
+  Typography,
+  CssBaseline,
+} from "@mui/material";
 import { blue, red } from "@mui/material/colors";
 
 /* const CustomSlider = styled(Slider)<SliderProps>(({theme})=>({                      
@@ -12,15 +21,14 @@ import { blue, red } from "@mui/material/colors";
     }                       
 })) */
 
-
 // Using Custom SliderProps
 
 type CustomSliderProps = SliderProps & {
-    error? : boolean
-}
+  error?: boolean;
+};
 
 const CustomSlider = styled(Slider, {
-    shouldForwardProp : (prop) => prop != "error"
+  shouldForwardProp: (prop) => prop != "error",
 })<CustomSliderProps>(({ theme, error }) => ({
   width: theme.spacing(10), // 10 * 8px ==> 80px
   color: theme.palette.success.main,
@@ -32,61 +40,74 @@ const CustomSlider = styled(Slider, {
   },
 }));
 
-
-
-// Overriding Global theme 
+// Overriding Global theme
 const theme = createTheme({
-    typography:{
-        // fontFamily:"-apple-system",
+  typography: {
+    // fontFamily:"-apple-system",
 
-        button:{
-            // fontSize:"9rem",
-            fontStyle: "italic"
-        }
+    button: {
+      // fontSize:"9rem",
+      fontStyle: "italic",
     },
+  },
 
-    components:{
-        MuiButton:{
-            defaultProps:{
-                // disableRipple: true,
-                variant:"contained"
+  components: {
+    MuiButton: {
+      defaultProps: {
+        // disableRipple: true,
+        variant: "contained",
+      },
+      styleOverrides: {
+        root: {
+          fontSize: "2rem",
+          variants: [
+            {
+              props: {
+                variant: "outlined",
+                color: "secondary",
+              },
+              style: {
+                fontSize: ".5rem",
+              },
             },
-            styleOverrides:{
-                root:{
-                    fontSize:'2rem',
-                    variants:[{
-                        props:{
-                            variant:"outlined",
-                            color:"secondary"
-                        },
-                        style:{
-                            fontSize:".5rem"
-                        }
-                    },
-                    {
-                        props:{variant:"dashed"},
-                        style:{border:`4px dashed ${red[500]}`}
-                    },
-                    {
-                        props: (props) => props.variant == 'dashed' && props.color != "secondary",
-                        style:{
-                            border:`2px dashed ${blue[500]}`
-                        }
-                    }
-                ]
+            {
+              props: { variant: "dashed" },
+              style: { border: `4px dashed ${red[500]}` },
+            },
+            {
+              props: (props) =>
+                props.variant == "dashed" && props.color != "secondary",
+              style: {
+                border: `2px dashed ${blue[500]}`,
+              },
+            },
+          ],
+        },
+      },
+    },
+    MuiCssBaseline: {
+      styleOverrides: (theme) => `
+
+                h1 {
+                    color: ${theme.palette.success.main}
                 }
-            }
-        }
-    }
-})
+            `,
+    },
+  },
+});
 
 const CustomizedComponent = () => {
   return (
     <ThemeProvider theme={theme}>
-      <CustomSlider error={true}/>
+      <CssBaseline />
+      <CustomSlider error={true} />
       <Button>Submit</Button>
-      <Button variant="outlined" color="secondary">Outlined Button</Button>
+      <Button variant="outlined" color="secondary">
+        Outlined Button
+      </Button>
       <Button variant="dashed">Dashed Button</Button>
+      <Typography variant="h1">H1</Typography>
+      <Typography variant="h2">H2</Typography>
     </ThemeProvider>
   );
 };
