667bb7d20b69102d118405593305f5f4176db39b
Nazil1712 <ndhalwala@gmail.com>
2026-05-08 15:11:50 +0530

    Responsive Components

--- src/Components/ResponsiveNess.tsx
+++ src/Components/ResponsiveNess.tsx
@@ -1,26 +1,86 @@
-import { Button, Grid, Typography, Container } from "@mui/material";
+import {
+  Button,
+  Grid,
+  Typography,
+  Container,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+
+const theme = createTheme({
+  /* We can define  */
+  // breakpoints:{
+  //   values :{
+  //     mobile:0,
+  //     tablet: 640,
+  //     laptop: 1024,
+  //     desktop: 1200
+  //   }
+  // }
+  components: {
+    MuiButton: {
+      // variants:}
+    },
+  },
+});
+
+
+// Responsive tag 🙃
+theme.typography.h3 = {
+  fontSize: "1.2rem",
+  "@media (min-width:600px)": {
+    fontSize: "1.5rem",
+  },
+  [theme.breakpoints.up("lg")]: {
+    fontSize: "2.4rem",
+  },
+};
 
 const ResponsiveNess = () => {
   return (
-    <Container maxWidth="lg">
-      <Typography variant="h4">Learning Responsiveness</Typography>
-      <Grid container spacing={2}>
-        <Grid size={{xs:12, md:6, xl:4}}>
-          <Button variant="contained" fullWidth>1</Button>
-        </Grid>
-        <Grid size={{xs:12, md:6, xl:4}}>
-          <Button variant="contained" fullWidth>2</Button>
-        </Grid>
-        <Grid size={{xs:12, md:6, xl:4}}>
-          <Button variant="contained" fullWidth>3</Button>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="lg">
+        <Typography variant="h3">Learning Responsiveness</Typography>
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12, md: 6, xl: 4 }}>
+            <Button variant="contained" fullWidth>
+              1
+            </Button>
+          </Grid>
+          <Grid size={{ xs: 12, md: 6, xl: 4 }}>
+            <Button variant="contained" fullWidth>
+              2
+            </Button>
+          </Grid>
+          <Grid size={{ xs: 12, md: 6, xl: 4 }}>
+            <Button variant="contained" fullWidth>
+              3
+            </Button>
+          </Grid>
+
+          {/* This will automatically caluclate it's size accordingly... */}
+          <Grid size="grow">
+            <Button variant="contained" fullWidth>
+              4
+            </Button>
+          </Grid>
         </Grid>
 
-        {/* This will automatically caluclate it's size accordingly... */}
-        <Grid size="grow">
-          <Button variant="contained" fullWidth>4</Button>
-        </Grid>
-      </Grid>
-    </Container>
+        <Button
+          variant="contained"
+          sx={(theme) => ({
+            marginTop: 2,
+            backgroundColor: "green",
+            [theme.breakpoints.between("xs", "md")]: {
+              bgcolor: "yellow",
+              color: "black",
+            },
+          })}
+        >
+          Responsive Button
+        </Button>
+      </Container>
+    </ThemeProvider>
   );
 };
 
--- src/types/mui.d.ts
+++ src/types/mui.d.ts
@@ -7,3 +7,18 @@ declare module "@mui/material/Button" {
         dashed: true;
     }
 }
+
+
+// declare module "@mui/material/styles" {
+//     interface BreakpointOverrides {
+//         xs: false;
+//         sm: false;
+//         md: false;
+//         lg: false;
+//         xl: false;
+//         mobile:true;
+//         tablet:true;
+//         laptop:true;
+//         desktop:true;
+//     }
+// }
